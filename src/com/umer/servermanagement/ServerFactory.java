f77d16c4670e0298595b2c410b0df8bdfe5d054b
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2021-11-17 11:17:07 +0100

    Feature: Added comments for further explanation.

--- src/com/umer/servermanagement/ServerFactory.java
+++ src/com/umer/servermanagement/ServerFactory.java
@@ -6,18 +6,26 @@ import com.umer.servermanagement.servertypes.MailServer;
 
 /**
  * This class will pump out different types of server objects.
+ * 
  * @author umer
  *
  */
 public class ServerFactory {
-	
-	public static IServer getServer(String serverType) throws Exception{
+
+	public static IServer getServer(String serverType) throws Exception {
 		// This contains the logic of which server object to invoke.
-		switch(serverType) {
+		switch (serverType) {
 		case "mail":
 			return new MailServer();
 		case "ftp":
 			return new FTPServer();
+		/**
+		 * In case we have additional servers in the future such as APP Server or Test
+		 * Server, We can simply add another case here with a class that implements the
+		 * IServer interface.
+		 * 
+		 * 
+		 */
 		default:
 			throw new Exception("Invalid server type");
 		}
