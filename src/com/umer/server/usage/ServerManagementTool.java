416f4e7ff6a928021dc7042588af226c65c024e3
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2021-11-17 09:04:09 +0100

    Feature: Created a scaffolding in main method which simulates a running system which asks for the sanem of the server to be fixed.

--- src/com/umer/server/usage/ServerManagementTool.java
+++ src/com/umer/server/usage/ServerManagementTool.java
@@ -1,5 +1,17 @@
 package com.umer.server.usage;
 
+import java.util.Scanner;
+
 public class ServerManagementTool {
 
+	public static void main(String[] args) {
+		/**
+		 * This user input takes place of a GUI drop down, so that we can keep the
+		 * implementation simple and focus on the implementation and usage of Factory
+		 * pattern
+		 */
+		Scanner input = new Scanner(System.in);
+		System.out.println("Which server do you want to resolve?");
+		String serverName=input.nextLine();
+	}
 }
