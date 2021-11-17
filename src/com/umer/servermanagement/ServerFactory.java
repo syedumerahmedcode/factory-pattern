d6cfb9f376ecb2bfb3f9cfa947b51c28d28117ec
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2021-11-17 11:00:11 +0100

    Feature: Created the logic of request specific objects in the factory class.

--- src/com/umer/servermanagement/ServerFactory.java
+++ src/com/umer/servermanagement/ServerFactory.java
@@ -1,6 +1,8 @@
 package com.umer.servermanagement;
 
+import com.umer.servermanagement.servertypes.FTPServer;
 import com.umer.servermanagement.servertypes.IServer;
+import com.umer.servermanagement.servertypes.MailServer;
 
 /**
  * This class will pump out different types of server objects.
@@ -9,9 +11,15 @@ import com.umer.servermanagement.servertypes.IServer;
  */
 public class ServerFactory {
 	
-	public static IServer getServer(String serverType) {
-		// TODO: This will contain the logic of which server object to invoke
-		
+	public static IServer getServer(String serverType) throws Exception{
+		// This contains the logic of which server object to invoke.
+		switch(serverType) {
+		case "mail":
+			return new MailServer();
+		case "ftp":
+			return new FTPServer();
+		default:
+			throw new Exception("Invalid server type");
+		}
 	}
-
 }
