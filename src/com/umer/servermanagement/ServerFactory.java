a757b134b6c864298e6e87808752313bbe5d4ae6
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2021-11-17 09:41:00 +0100

    Feature: Created an interface for Server types.

--- src/com/umer/servermanagement/ServerFactory.java
+++ src/com/umer/servermanagement/ServerFactory.java
@@ -1,5 +1,7 @@
 package com.umer.servermanagement;
 
+import com.umer.servermanagement.servertypes.IServer;
+
 /**
  * This class will pump out different types of server objects.
  * @author umer
@@ -9,6 +11,7 @@ public class ServerFactory {
 	
 	public static IServer getServer(String serverType) {
 		// TODO: This will contain the logic of which server object to invoke
+		
 	}
 
 }
---
+++ src/com/umer/servermanagement/servertypes/IServer.java
@@ -0,0 +1,9 @@
+package com.umer.servermanagement.servertypes;
+
+public interface IServer {
+
+	/**
+	 * Resolves any network related issues for a specific server
+	 */
+	public void resolve();
+}
