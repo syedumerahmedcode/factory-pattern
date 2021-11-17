4f7c53dccddc8e08ed0898125d2fd156c72fd3a4
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2021-11-17 11:05:15 +0100

    Feature: Added resolution of server via factory in the class.

--- src/com/umer/servermanagement/usage/ServerManagementTool.java
+++ src/com/umer/servermanagement/usage/ServerManagementTool.java
@@ -2,16 +2,27 @@ package com.umer.servermanagement.usage;
 
 import java.util.Scanner;
 
+import com.umer.servermanagement.ServerFactory;
+import com.umer.servermanagement.servertypes.IServer;
+
 public class ServerManagementTool {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		/**
 		 * This user input takes place of a GUI drop down, so that we can keep the
 		 * implementation simple and focus on the implementation and usage of Factory
 		 * pattern
+		 * 
+		 * Helps us in determining what type of server does the user wants to resolve?
 		 */
 		Scanner input = new Scanner(System.in);
 		System.out.println("Which server do you want to resolve?");
-		String serverName=input.nextLine();
+		String serverName = input.nextLine();
+		
+		/**
+		 * We do not specify explicitly i the code which server to initialize. This is taken care of by the factory itself.
+		 */
+		IServer server=ServerFactory.getServer(serverName);
+		server.resolve();
 	}
 }
