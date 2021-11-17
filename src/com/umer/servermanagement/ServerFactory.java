e722bf68c460bd324c88762ee39b406147c87f5e
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2021-11-17 09:20:16 +0100

    Feature: Created an empty ServerFactory class.

--- src/com/umer/servermanagement/ServerFactory.java
+++ src/com/umer/servermanagement/ServerFactory.java
@@ -1,5 +1,14 @@
 package com.umer.servermanagement;
 
+/**
+ * This class will pump out different types of server objects.
+ * @author umer
+ *
+ */
 public class ServerFactory {
+	
+	public static IServer getServer(String serverType) {
+		// TODO: This will contain the logic of which server object to invoke
+	}
 
 }
